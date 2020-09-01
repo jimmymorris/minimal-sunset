a0d4d15bb65c8ca9e11655387adf03b99749b1f9
Jimmy Morris <jimmy.e.morris@gmail.com>
2020-08-31 23:32:44 -0400

    tweaks

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ import './App.css';
 const App = () => {
   return (
     <SettingsContextProvider>
-      <div style={{ display: 'flex', height: '100%'}}>
+      <div style={{ display: 'flex', height: '100vh'}}>
         <div style={{ padding: '32px', flex: '75%' }}>
           <Drawing />
         </div>
-        <div style={{ padding: '32px', flex: '25%', background: '#eee' }}>
+        <div style={{ padding: '32px', flex: '25%', background: '#eee', boxShadow: '-5px 0px 36px -18px rgba(0,0,0,0.75)' }}>
           <Controls />
         </div>
       </div>
--- src/components/Drawing/index.js
+++ src/components/Drawing/index.js
@@ -9,7 +9,7 @@ const Drawing = () => {
   const maxY = height - bleed;
 
   useEffect(() => {
-    console.log({bleed})
+    console.log({ bleed })
     let v = 0;
     let y = middleY;
     setLines([])
@@ -23,58 +23,44 @@ const Drawing = () => {
     }
   }, [base, bleed, width, maxY, middleY, ratio])
 
-  const aspectPercent = (height / width) * 100;
-
-  const wrapperCSS = {
-    height: 0,
-    overflow: 'hidden',
-    padding: `${aspectPercent}% 0 0`,
-    position: 'relative'
-  }
-
   const svgCSS = {
-    position: 'absolute',
-    left: '50%',
-    marginLeft: '-50%',
-    top: 0,
+    maxHeight: `calc(100vh - ${bleed}px)`
   }
 
   return (
-    <div style={wrapperCSS}>
-      <svg viewBox={`0 0 ${width} ${height}`} style={svgCSS} xmlns="https://www.w3.org/2000/svg">
-        <rect
-          id="background"
-          height="100%"
-          fill={backgroundColor}
-        />
-        <ellipse
-          id="sun"
-          cx={sunX}
-          cy={sunY}
-          rx={sunRadius}
-          ry={sunRadius}
-          fill={strokeColor}
-        />
-        <rect
-          x={lines.length > 0 ? lines[0].x1 : 0}
-          y={lines.length > 0 ? lines[0].y1 : height / 2}
-          width={width - (bleed * 2)}
-          height={lines.length > 0 ? (height / 2 - bleed) : 0}
-          fill={backgroundColor}
-        />
-        <rect
-          id="border"
-          x={bleed}
-          y={bleed}
-          width={width - (bleed * 2)}
-          height={height - (bleed * 2)}
-          stroke={strokeColor}
-          strokeWidth={strokeWidth}
-          fill="none"
-        />
-        {lines.map((l, index) => <line key={`line-${index}`} x1={l.x1} y1={l.y1} x2={l.x2} y2={l.y2} stroke={strokeColor} strokeWidth={strokeWidth} />)}
-      </svg>
-    </div>
+    <svg viewBox={`0 0 ${width} ${height}`} style={svgCSS} width="100%" xmlns="https://www.w3.org/2000/svg">
+      <rect
+        id="background"
+        height="100%"
+        fill={backgroundColor}
+      />
+      <ellipse
+        id="sun"
+        cx={sunX}
+        cy={sunY}
+        rx={sunRadius}
+        ry={sunRadius}
+        fill={strokeColor}
+      />
+      <rect
+        x={lines.length > 0 ? lines[0].x1 : 0}
+        y={lines.length > 0 ? lines[0].y1 : height / 2}
+        width={width - (bleed * 2)}
+        height={lines.length > 0 ? (height / 2 - bleed) : 0}
+        fill={backgroundColor}
+      />
+      <rect
+        id="border"
+        x={bleed}
+        y={bleed}
+        width={width - (bleed * 2)}
+        height={height - (bleed * 2)}
+        stroke={strokeColor}
+        strokeWidth={strokeWidth}
+        fill="none"
+      />
+      {lines.map((l, index) => <line key={`line-${index}`} x1={l.x1} y1={l.y1} x2={l.x2} y2={l.y2} stroke={strokeColor} strokeWidth={strokeWidth} />)}
+    </svg>
   );
 
 }
